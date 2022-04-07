809002b50cd832210b0b993f27d4ba35f4b07380
Tryrix <Tryrix@gmail.com>
2022-04-08 01:01:11 +0300

    2

--- 1.py
+++ 1.py
@@ -5,18 +5,12 @@ filename = '1.xlsx'
 wb = openpyxl.load_workbook(filename)
 sheet = wb.active
 k = 0
-for n in sheet['A']:
-    k += 1
-    a = sheet[f'A{n.row}'].value
-    b = sheet[f'B{n.row}'].value
-    c = sheet[f'C{n.row}'].value
-    e = sheet[f'D{n.row}'].value
-    a = str(a)
-    b = str(b)
-    c = str(c)
-    e = str(e)
-    sheet[f'E{n.row}'].value = f'{a}_{b}_{c}_{e}'
-    print(a, b, c, e)
-print(k)
+for n in sheet['E']:
+    sheet[f'F{n.row}'].value = sheet[f'E{n.row}'].value
+for n in sheet['E']:
+    for k in sheet['E']:
+        if n.value == k.value and n.value != None:
+            print(n.value, k.value)
+            sheet.delete_rows(n.row)
 wb.save("1.xlsx")
 # print(m)
